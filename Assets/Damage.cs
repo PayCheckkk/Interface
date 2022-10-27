e6a074557fa1462714f1789ee84053ee6fa5882d
PayCheckkk <113245313+PayCheckkk@users.noreply.github.com>
2022-10-27 17:04:20 +0700

    Add files via upload

--- Assets/Damage.cs
+++ Assets/Damage.cs
@@ -8,20 +8,17 @@ public class Damage : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
 
-    private float _currentValue;
     private float _targetValue;
     private float _damage = 10;
 
     private void Update()
     {
-        _currentValue = _slider.value;
-        _targetValue = _currentValue - _damage;
+        _targetValue = _slider.value - _damage;
     }
 
     public void OnButtonClick()
     {
         _slider.DOValue(_targetValue, 0.3f);
-        _currentValue = _targetValue;
         Debug.Log(_slider.value);
     }
 }
--- Assets/Heal.cs
+++ Assets/Heal.cs
@@ -8,20 +8,17 @@ public class Heal : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
 
-    private float _currentValue;
     private float _targetValue;
     private float _heal = 10;
 
     private void Update()
     {
-        _currentValue = _slider.value;
-        _targetValue = _currentValue + _heal;
+        _targetValue = _slider.value + _heal;
     }
 
     public void OnButtonClick()
     {
         _slider.DOValue(_targetValue, 0.3f);
-        _currentValue = _targetValue;
         Debug.Log(_slider.value);
     }
 }
